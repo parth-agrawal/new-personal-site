31a850ea9a5ca3cb7d055f0c6c85e7c8ca3d0c83
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-22 18:39:10 -0400

    setup complete, creating homepage

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+  return (
+    <>
+      <div className="flex flex-col py-48 px-12">
+        <div className="flex justify-center items-center h-[500px] bg-[#FFF7AF]">
+          <div className="text-4xl w-[50%] text-center">
+            Hi, I'm Parth. I build, write, and create.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+// MainFace
+// SubFace
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Portfolio } from "./components/Portfolio.tsx";
+import { Router } from "express";
+import { Home } from "./components/Home.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/portfolio",
+    element: <Portfolio />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
