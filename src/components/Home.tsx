9ab1d2db16d65f24f9f1f40bb4bddbdac98c81ef
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-22 18:49:49 -0400

    navbar styling basic

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,11 @@
 import { MainFace } from "./MainFace";
+import { Navbar } from "./Navbar";
 
 export const Home = () => {
   return (
+    // move navbar to a layout component
     <>
+      <Navbar />
       <MainFace content="Hi, I'm Parth. I build, write, and create." />
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,52 @@
-export const Navbar = () => {};
+import { Link } from "react-router-dom";
+
+type NavbarLinkData = {
+  name: string;
+  path: string;
+  color: string;
+};
+
+const navbarLinks: NavbarLinkData[] = [
+  {
+    name: "Parth Agrawal",
+    path: "/",
+    color: "#DDDDDD",
+  },
+  {
+    name: "Portfolio",
+    path: "/portfolio",
+    color: "#FFAFAF",
+  },
+];
+
+export const Navbar = () => {
+  return (
+    <>
+      <div className="flex flex-row">
+        {navbarLinks.map((link) => (
+          <NavbarLink
+            key={link.name}
+            name={link.name}
+            path={link.path}
+            color={link.color}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const NavbarLink = ({ name, path, color }: NavbarLinkData) => {
+  return (
+    <>
+      <Link to={path}>
+        <div
+          style={{ backgroundColor: color }}
+          className={`px-4 mx-2 py-2 text-md font-bold`}
+        >
+          {name}
+        </div>
+      </Link>
+    </>
+  );
+};
