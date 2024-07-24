23006a8fede985742797462e1ec29ef29e75eae1
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-24 17:15:48 -0400

    layout refactor

--- src/components/FullFolder.tsx
+++ src/components/FullFolder.tsx
@@ -1,22 +1,37 @@
+import React from "react";
 import { Divider } from "./Divider";
 import { Navbar } from "./Navbar";
 
-interface MainFaceProps {
-  content: string;
+type FolderLayoutProps = {
+  children: [string, React.ReactNode]
+};
+
+type PageContent = {
+  mainFaceContent: string;
+  subFaceContent: string;
 }
 
-export const FullFolder = ({ content }: MainFaceProps) => {
+
+export const FolderLayout = ({ children }: FolderLayoutProps) => {
+  const mainContent = children[0];
+  const subcontent = children[1];
   return (
     <>
       <div className="flex flex-col py-48 px-12 font-mono">
         <Navbar />
         <div className="flex justify-center items-center h-[500px] bg-[#FFF7AF]">
           <div className="text-4xl w-[75%] font-bold text-center">
-            {content}
+            {mainContent}
           </div>
         </div>
+
         <Divider />
+        <div className="flex justify-center items-center min-h-[500px] bg-[#FFF7AF] px-10">
+          {subcontent}
+        </div>
       </div>
     </>
   );
 };
+
+
