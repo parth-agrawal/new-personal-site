dc478f9f141b4ef38f43098c58369ee2ba396be3
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-26 16:04:45 -0400

    styling

--- frontend/src/components/Portfolio.tsx
+++ frontend/src/components/Portfolio.tsx
@@ -41,9 +41,11 @@ const PortfolioSubpage = () => {
 
   return (
     <>
-      <div className="flex flex-row w-full justify-between">
+      <div className="flex flex-wrap w-full justify-around gap-20">
         {projects && projects.map((project, index) => (
-          <PortfolioEntry key={index} project={project} />
+          <div className="w-84 p-2" key={index}>
+            <PortfolioEntry project={project} />
+          </div>
         ))}
       </div>
 
@@ -53,18 +55,20 @@ const PortfolioSubpage = () => {
 }
 
 const PortfolioEntry = ({ project }: { project: Project }) => {
-  const { title, image, url, github } = project;
+  const { title, description, image, url, github } = project;
   return (
     <div className="flex gap-2 flex-col bg-[#FFAFD5] p-12 items-center justify-center">
       <div className="flex  w-full h-auto justify-center">
         <img className="border flex min-w-24 max-h-48" src={image} />
       </div>
-      <div className="pt-2 text-2xl font-BadScript font">
+      <div className="pt-2 text-2xl font-BadScript">
 
         {title}
       </div>
+      <div className="text-lg font-BadScript">
+        {description}
+      </div>
       <div className="flex flex-col">
-
         <a href={url} className="underline hover:text-[#B1AFFF]">Visit</a>
         <a href={github} className="underline hover:text-[#B1AFFF]">Github</a>
       </div>
